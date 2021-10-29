687c014c953fcb22b42f83d568722830be2d77d1
aliriomiguel <aliriomiguel@gmail.com>
2021-10-29 14:01:00 -0400

    first commit - first scene built, spartan warrior first movements added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    [SerializeField]
+    private float _runSpeed = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * _runSpeed;
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
